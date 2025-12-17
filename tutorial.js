7cddcb750895245699960d69fb4746ae7458eeb1
Uday <udayagarwal234@gmail.com>
2025-12-17 13:36:17 +0530

    Add hello and tutorial files

---
+++ tutorial.js
@@ -0,0 +1,19 @@
+/*
+
+DevOps -> 
+
+Ship readable code through GitHub
+
+Build Stage -> the code needs to be compiled ot transpiled, dependencies installed, bundled or packaged(in docker) and check for linting or security mistakes. 
+
+We build docker images, run containers and build them every time we make a commit,
+
+then we do testing. Automated test run as part of CI pipeline. 
+
+Kubernetes has rolling deployment. 
+
+CI/CD -> Continuous Integration & Continuous Deployment. 
+
+*/ 
+
+console.log('test')
