5ee5776ac2bae4a7d476ef872b78a242f1b99376
Uday <udayagarwal234@gmail.com>
2025-12-19 13:48:09 +0530

    feat: add CI pipeline

--- tutorial.js
+++ tutorial.js
@@ -14,6 +14,21 @@ Kubernetes has rolling deployment.
 
 CI/CD -> Continuous Integration & Continuous Deployment. 
 
+yaml file -> This is where the automation code is inside our repo ao it is easy to audit and reuse. It is simple to use. 
+
+yaml has some special keywords -> 
+    name - Used to define workflows or job title
+    on - defined triggers such as push, pr or schedules
+    jobs - define jobs, their OS and steps. jobs are a a group of steps that run together
+    steps - sequential commands and actions. They are inside jobs and are the real instructions that sre to be executed one after another. 
+    run - shell commands to execute
+    uses - use prebuilt actions
+    with - pass parameters to actions
+    env - set environment variables
+    needs - make one job depend on another
+
+
+
 */ 
 
 console.log('test')
